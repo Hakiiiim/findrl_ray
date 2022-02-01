680d83625f69ba6e95a912b6887435771da0d250
gibsonlin <gibsonlin@icloud.com>
2022-02-01 12:49:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from cmath import pi
 import streamlit as st
 import json
 
@@ -9,12 +10,6 @@ st.sidebar.markdown("# MBBR Type")
 mbbr_type = st.sidebar.selectbox("Select build type",["Conventional","Microbeads"],0)
 peak_biomass = st.sidebar.slider('Peak Biomass(kg/m3)')
 #Main Configs
-
-
-
-
-
-
 if nav_page == "Dashboard":
     with open('config.json') as json_file:
         config_data = json.load(json_file)
@@ -29,13 +24,23 @@ if nav_page == "KPIs":
 if nav_page == "CostFactors":
     tank_costs = st.slider("RAS Tank Costs(TWD/m3)", min_value=0, max_value=1000, value=600)
     microbeads_ssa = st.select_slider('Beads Media SSA',[1280,1800,2500,3500])
+    tank_depth = st.slider("Tank Culture Depth(m)",min_value=1.0,max_value=1.5, step=0.1, value=1.2)
+    tank_diameter = st.slider("Tank Diameter",min_value=6.0,max_value=9.0,step=1.0, value=6.0)
+    tank_area = ((tank_diameter*0.5)^2)*pi()
+    tank_volume = tank_area*tank_depth
+    static_o2_consumptions = st.slider()
+
 
     nav_config = {
             "microbeads_ssa": microbeads_ssa, 
-            "tank_costs": tank_costs
+            "tank_costs": tank_costs,
+            "tank_depth":tank_depth,
+            "tank_diameter":tank_diameter,
+            "tank_area":tank_area,
+            "tank_volume":tank_volume,
+            "static_o2_consumptions":static_o2_consumptions,
+
     }
-    config = json.dumps(nav_config)
-    st.write('json test',config)
 
     with open('config.json', 'w') as outfile:
         json.dump(nav_config, outfile)

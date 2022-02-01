c212ab0cfe2ef2470e09c852431ab152164f1f56
gibsonlin <gibsonlin@icloud.com>
2022-02-01 16:25:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import pandas as pd
 from sympy import expand 
 st.set_page_config(layout="wide")
 
-col1, col2, col3, col4 = st.columns([1.2,0.85,2.5,0.85])
+col1, col2, col3, col4 = st.columns([1.1,1,2.3,0.85])
 
 st.sidebar.title("Tilapia Culture Economics Prophet BI")
 #Sidebar Controls
@@ -48,6 +48,13 @@ with col1:
         stage_3_den = st.slider('Stage 3 Densities',min_value=80,max_value=400,step=25,value=150)
         stage_4_den = st.slider('Stage 4 Densities',min_value=50,max_value=250,step=25,value=100)
         stage_5_den = st.slider('Stage 5 Densities',min_value=20,max_value=150,step=10,value=60)
+    #Not expanded stage mortalitiess
+    with st.expander('Mortalities Each Stage', expanded=False):
+        stage_1_mort = st.number_input('Stage 1 Mortalities: (%)',value=3)
+        stage_2_mort = st.number_input('Stage 2 Mortalities: (%)',value=2)
+        stage_3_mort = st.number_input('Stage 3 Mortalities: (%)',value=1.5)
+        stage_4_mort = st.number_input('Stage 4 Mortalities: (%)',value=1)
+        stage_5_mort = st.number_input('Stage 5 Mortalities: (%)',value=1)
 
 def tank_volume_fn():
     x = round(tank_depth *float(tank_diameter*0.5)*float(tank_diameter*0.5)*3.1415,3)
@@ -70,6 +77,21 @@ def stage_4_adg():
 def stage_5_adg():
     x =(harvest_weight-stage_4_abw)/stage_5_days
     return x
+def stage_1_biomass():
+    x = ((stage_1_den*stage_1_abw)/1000)
+    return x
+def stage_2_biomass():
+    x = stage_2_den*stage_2_abw*(100-stage_2_mort)/1000
+    return x
+def stage_3_biomass():
+    x = stage_3_den*stage_3_abw*(100-stage_3_mort)/1000
+    return x
+def stage_1_biomass():
+    x = stage_4_den*stage_4_abw*(100-stage_4_mort)/1000
+    return x
+def stage_1_biomass():
+    x = stage_5_den*stage_5_abw*(100-stage_5_mort)/1000
+    return x
 
 with col2:
     st.header('KPIs')
@@ -84,6 +106,11 @@ with col2:
         st.write('Stage 4 ADG: {0: .3f}(g/day)'.format(stage_4_adg()))
         st.write('Stage 5 ADG: {0: .3f}(g/day)'.format(stage_5_adg()))
     with st.expander('Biomass Per Stage',expanded=False):
+        st.write('Stage 1 Peak Biomass: {0: .2f}(kg/m3)'.format(stage_1_biomass()))
+
+
+
+
 
 with col3:
     st.header('Charts Tables')

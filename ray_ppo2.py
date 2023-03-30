046c4066624360610230ecb396bd0f0c31a42f59
gibsonlin <gibsonlin@icloud.com>
2023-03-30 09:17:21 +0800

    2 VMs

--- ray_ppo2.py
+++ ray_ppo2.py
@@ -91,7 +91,7 @@ print(f"ray is being initialized")
 config = ppo.PPOConfig()  
 config = config.training(gamma=0.9, lr=0.001, kl_coeff=0.3)  
 config = config.resources(num_gpus=0)  
-config = config.rollouts(num_rollout_workers=4)
+config = config.rollouts(num_rollout_workers=32)
 
 # registering the environment to ray
 register_env("finrl", env_creator)

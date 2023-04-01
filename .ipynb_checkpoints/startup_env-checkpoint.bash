a9588103c6ebbddd45000f4b14c1a0fb97a703f6
gibsonlin <gibsonlin@icloud.com>
2023-04-01 17:55:56 +0800

    Updated train/evaluate

--- .ipynb_checkpoints/startup_env-checkpoint.bash
+++ .ipynb_checkpoints/startup_env-checkpoint.bash
@@ -10,7 +10,7 @@ pip install swig
 git clone https://github.com/Hakiiiim/FinRL.git
 
 # install necessary dependencies
-sudo apt-get update && sudo apt-get install -y cmake libopenmpi-dev python3-dev zlib1g-dev libgl1-mesa-glx swig
+#sudo apt-get update && sudo apt-get install -y cmake libopenmpi-dev python3-dev zlib1g-dev libgl1-mesa-glx swig
 
 # install setuptools
 pip install setuptools==65.5.0
@@ -23,3 +23,6 @@ pip install FinRL/.
 
 # install gymnasium
 pip install gymnasium
+
+#export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
+#pip install numpy==1.21

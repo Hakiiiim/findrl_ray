e8d2f858f4c345b1162ca2d4f5bc93604a0f923c
gibsonlin <gibsonlin@icloud.com>
2023-04-02 22:03:20 +0800

    Updated Evaluate Notebook

--- startup_env.bash
+++ startup_env.bash
@@ -16,7 +16,7 @@ pip install git+https://github.com/AI4Finance-Foundation/FinRL.git
 sudo apt-get update && sudo apt-get install -y cmake libopenmpi-dev python3-dev zlib1g-dev libgl1-mesa-glx swig
 
 # install gymnasium
-pip install gymnasium stockstats dm_tree
+pip install gymnasium stockstats dm_tree scikit-image tqdm
 
 #export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
 #pip install numpy==1.21

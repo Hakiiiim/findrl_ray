f4fcf0676ce53470471c59861502ff1d2bd890ce
gibsonlin <gibsonlin@icloud.com>
2023-04-02 23:19:49 +0800

    updated saving script

--- ray_ppo2.py
+++ ray_ppo2.py
@@ -74,14 +74,13 @@ def env_creator(env_config):
 ray.shutdown()
 print(f"ray is being initialized")
 config = ppo.PPOConfig()  
-config = config.training(gamma=0.9, lr=0.00025, kl_coeff=0.3)  
+config = config.training(gamma=0.9, lr=0.001, kl_coeff=0.3)  
 config = config.resources(num_gpus=0)  
 config = config.rollouts(num_rollout_workers=num_workers)
 config = config.framework(framework="torch")
 config['seed'] = 42
 config["model"]["fcnet_hiddens"] = [256, 256, 256, 32]
 config['train_batch_size'] = 10240
-
 
 # registering the environment to ray
 register_env("finrl", env_creator)
@@ -104,9 +103,11 @@ while ep <= total_episodes:
         rwd = results[-1]['episode_reward_mean']
         print(f'Mean Rwd:{rwd}')   
     print(f'Current episode{ep} \nTime/Its:{time.time()-start:.2f}s')
-    if ep % 100 == 0:
+    if ep % 25 == 0:
         cwd_checkpoint = f"results/{agent_name}_{date}_{ep}"
         trainer.save(cwd_checkpoint)
         print(f"Checkpoint saved in directory {cwd_checkpoint}")
         
 print(f'Complete training job took{time.time()-job_time:.2f}s')
+cwd_checkpoint = f"results/{date}_{ppo}_{ep}"
+trainer.save(cwd_checkpoint)
